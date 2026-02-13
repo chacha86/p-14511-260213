ac10ac53f4a4b5abc7433babbb324b49bb72941f
chacha86 <taejincha0619@gmail.com>
2026-02-13 15:48:32 +0900

    10

---
+++ src/main/java/com/back/domain/wiseSaying/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.back.domain.wiseSaying.repository;
+
+import com.back.domain.wiseSaying.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 인터페이스 ==> 모든 메서드가 추상메서드인 추상 클래스
+public interface PostRepository extends JpaRepository<Post, Integer> {}
